8cc565cb85dd9cb5eb01c2a0e4a1cd920233b178
aquairus <18650013469@163.com>
2016-05-04 20:25:36 +0800

    save

--- hw_2/main.js
+++ hw_2/main.js
@@ -1,83 +1,60 @@
+require.config({　　　　
+    paths: {　　　　　　
+        "jquery": "../lib/jquery",
 
-require.config({
-　　　　paths: {
-　　　　　　"jquery": "../lib/jquery",
+        　　　　
+    }　　
+});
 
-　　　　}
-　　});
+require(['jquery'], function($) {
 
-require(['jquery'], function ($){
 
-      // /alert('ready');
+    $('#color').bind("click", function c() {
+        colorMod = cp2.colorpicker('getValue', '#00aabb')
+        ctx.strokeStyle = colorMod
 
-      $('#color').bind("click", function c(){
-        colorMod=cp2.colorpicker('getValue','#00aabb')
-        ctx.strokeStyle  = colorMod
-        //
-      }
-      );
+    });
 
-      $('#clean').bind("click", function clean(){
-        ctx.clearRect(0,0,400,400);
-        //ctx.putImageData(tmp_data,0,0);
-      }
-      );
-      $('#restore').bind("click", function restore(){
+    $('#clean').bind("click", function clean() {
+        ctx.clearRect(0, 0, 400, 400);
 
-      //  ctx.clearRect(0,0,400,400);
-      tmp_data=pic_heap.pop()
-        ctx.putImageData(tmp_data,0,0);
-      }
-      );
-      $('#save').bind("click", function save(){
-        img_url=canvas.toDataURL("image/png");
+    });
+    $('#restore').bind("click", function restore() {
+
+
+        tmp_data = pic_heap.pop()
+        ctx.putImageData(tmp_data, 0, 0);
+    });
+    $('#save').bind("click", function save() {
+        img_url = canvas.toDataURL("image/png");
         pic = window.open(img_url, "canvas_pic")
-      }
-      );
+    });
 
 
 
 
-      $('#div1').mousedown( function start(event){
+    $('#div1').mousedown(function start(event) {
         ctx.beginPath();
 
-        if(flag == 0){
-          tmp_data=ctx.getImageData(0,0,400,400)
-          pic_heap.push(tmp_data)
-          flag = 1;
+        if (flag == 0) {
+            tmp_data = ctx.getImageData(0, 0, 400, 400)
+            pic_heap.push(tmp_data)
+            flag = 1;
 
         }
-      //  tmp_data=ctx.getImageData(0,0,400,400)
-      //  startpoint(event)
-      }
-      );
 
-      $('#div1').mousemove( function move(event){
+    });
+
+    $('#div1').mousemove(function move(event) {
         a(event)
-      }
-      );
+    });
 
-      $('#div1').mouseup( function end(event){
+    $('#div1').mouseup(function end(event) {
 
         endpoint(event)
 
-      }
-      );
+    });
 
 
-      // $(function() {
-      //
-      //   $('#cp2').colorpicker();
-      //     alert('welcome')
-      // });
-
-      // $('.color').bind("click", function c(){
-      //   alert('HI')}
-      // );
-      // $().ready(function orz(){
-      //   alert('welcome')}
-      // );
-      // )
-
-
-　　});
+    　　
+});
